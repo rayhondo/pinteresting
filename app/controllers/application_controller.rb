68fafb1e56d8f877fb0a43d479b0244c31bdb6e6
Ray Kim <rkim419@gmail.com>
2017-01-07 14:33:16 -0800

    Add handles to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-end
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  	protected
+	  	def configure_permitted_parameters
+	  		devise_parameter_sanitizer.permit(:sign_up, keys: [:handle])
+	 		devise_parameter_sanitizer.permit(:account_update, keys: [:handle])
+		end
+	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :pins
+
+  validates :name, presence: true
+
 end
