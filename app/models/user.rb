30454a387cec92ef75e809483c364ab826c1cb9d
Ray Kim <rkim419@gmail.com>
2017-01-07 16:48:37 -0800

    fixing bug with deleted users pins showing up and showing edit/delete icons when there is no user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
 
   validates :handle, presence: true
 
