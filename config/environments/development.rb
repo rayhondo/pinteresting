7bb3d861e1b86f1d709ef9d32af86136532d95f8
Ray Kim <rkim419@gmail.com>
2017-01-07 16:19:08 -0800

    wrapping up the email send functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,14 +55,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    domain: 'localhost',
-    authentication: “plain”,
-    enable_starttls_auto: true,
-    user_name: 'app57049954@heroku.com',
-    password: 'ger58vt10985'
+  config.action_mailer.default_url_options = { :host => 'www.justdoobie.com' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
   }
 
   config.paperclip_defaults = {
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'help@justdoobie.com'
+  config.mailer_sender = 'crutch@justdoobie.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
